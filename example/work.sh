74457788938a303dc69e1951f14446ba7435f190
wxhyihuan <wxh244295043@qq.com>
2018-03-12 17:05:46 +0800

    change file path
    change file path

--- example/work.sh
+++ example/work.sh
@@ -1,5 +1,5 @@
-java -jar /prt1/puluotong/PMO/wangxh/Software/src/WorkflowDL/wdltool-0.14.jar inputs PairendFqtoUnmappedBam.wdl >PairendFqtoUnmappedBam.test4.json
+java -jar ~/WorkflowDL/wdltool-0.14.jar inputs PairendFqtoUnmappedBam.wdl >PairendFqtoUnmappedBam.json
 #修改WDL_fqfilter.test4.json
 #注意 fqtobammetainfo 中的run_date 日期格式为 2018-02-09 形式(其他形式如 2018/02/09 在GATK中是无法识别使用的)
 
-java -jar /prt1/puluotong/PMO/wangxh/Software/src/WorkflowDL/cromwell-30.1.jar run PairendFqtoUnmappedBam.wdl --inputs PairendFqtoUnmappedBam.test4.json
+java -jar ~/WorkflowDL/cromwell-30.1.jar run PairendFqtoUnmappedBam.wdl --inputs PairendFqtoUnmappedBam.json
